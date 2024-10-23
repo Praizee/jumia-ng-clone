575416f2eaa7056e70176c9cb190402c3d068d36
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-10-23 01:24:30 +0100

    update homepage bg

--- app/(homepage)/page.js
+++ app/(homepage)/page.js
@@ -33,41 +33,45 @@ import {
 
 export default function Home() {
   return (
-    <section className="bg-[#f1f1f2] relative min-h-screen">
-      <CTA />
-      <div className="xl:max-w-[1200px] max-w-[950px] mx-auto space-y-4 p-4 lg:px-0">
-        <Hero />
-        <FeaturedCategories />
-        <FlashSales />
-        <FeaturedCategories2 />
-        <SponsoredProducts />
-        <DealsToExpect />
-        <EarlyBirdDeals />
-        <TopSellingItems />
+    // <section className="bg-[#f1f1f2] relative min-h-screen">
+    <section className="bg-[#f1f1f2] md:bg-[#AADEF1] relative min-h-screen">
+      {/* <section className="min-h-screen md:bg-[url(https://ng.jumia.is/cms/0-1-weekly-cps/0-2024/Week_17/Pay-Day-Super-Saver/-BG-TAKEOVER_PAYDAY.jpg)] bg-contain bg-no-repeat"> */}
+      <section className="min-h-screen md:bg-[url(https://ng.jumia.is/cms/0-1-weekly-cps/0-2024/Brand-days/October/Nivea-Super-Brand-Day/BG.jpg)] bg-contain bg-no-repeat">
+        <CTA />
+        <div className="xl:max-w-[1200px] max-w-[950px] mx-auto space-y-4 p-4 lg:px-0">
+          <Hero />
+          <FeaturedCategories />
+          <FlashSales />
+          <FeaturedCategories2 />
+          <SponsoredProducts />
+          <DealsToExpect />
+          <EarlyBirdDeals />
+          <TopSellingItems />
 
-        <Banner2 />
-        <LimitedStockDeals />
-        <EarlyShoppersDeals />
-        <HalfPriceStore />
-        <Banner3 />
-        <EverythingMustGo />
-        <BinatoneWeek />
-        <BinatoneWeek2 />
-        <OfficialStores />
-        <NiveaOfficialStore />
-        <BeautyBargain />
-        <BeautyBargains />
-        <Appliances />
-        <AppliancesDeals />
-        <PhoneTabletDeals />
-        <PhoneDeals />
-        <FashionForward />
-        <FashionForwardDeals />
-        <TopSearched />
-        <Handpicked />
+          <Banner2 />
+          <LimitedStockDeals />
+          <EarlyShoppersDeals />
+          <HalfPriceStore />
+          <Banner3 />
+          <EverythingMustGo />
+          <BinatoneWeek />
+          <BinatoneWeek2 />
+          <OfficialStores />
+          <NiveaOfficialStore />
+          <BeautyBargain />
+          <BeautyBargains />
+          <Appliances />
+          <AppliancesDeals />
+          <PhoneTabletDeals />
+          <PhoneDeals />
+          <FashionForward />
+          <FashionForwardDeals />
+          <TopSearched />
+          <Handpicked />
 
-        <ShoppingDestination />
-      </div>
+          <ShoppingDestination />
+        </div>
+      </section>
     </section>
   );
 }
