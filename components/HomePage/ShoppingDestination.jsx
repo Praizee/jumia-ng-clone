b881bcd5c22307c8dca2f2f586dc5e9e4dd4fbfb
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-10-23 01:34:16 +0100

    chore: links

--- components/HomePage/ShoppingDestination.jsx
+++ components/HomePage/ShoppingDestination.jsx
@@ -231,27 +231,61 @@ const ShoppingDestination = () => {
       </h3>
       <p>
         Shopping on Jumia guarantees that you get the latest mobile phones,
-        tablets, or phablets before anyone else. Buy trusted Apple iOS iPhones
-        and iPads and quality Android brands like Samsung, Tecno, Xiaomi,
-        Infinix, and many others! Unlock the best functionality of your
-        smartphones when you buy our amazing phone accessories like AirPods,
-        earphones, powerbank, Bluetooth speakers, durable phone cases, and many
-        more! We have an endless supply of Jumia phone accessories from top
-        brands like Oraimo, Edifier, Samsung, Tecno, JBL, etc.
+        tablets, or phablets before anyone else. Buy trusted{" "}
+        <Link href="" className="underline font-semibold">
+          Apple
+        </Link>{" "}
+        iOS{" "}
+        <Link href="" className="underline font-semibold">
+          iPhones
+        </Link>{" "}
+        and iPads and quality Android brands like{" "}
+        <Link href="" className="underline font-semibold">
+          Samsung
+        </Link>
+        , Tecno, Xiaomi, Infinix, and many others! Unlock the best functionality
+        of your{" "}
+        <Link href="" className="underline font-semibold">
+          smartphones
+        </Link>{" "}
+        when you buy our amazing phone accessories like AirPods, earphones,
+        powerbank, Bluetooth speakers, durable phone cases, and many more! We
+        have an endless supply of Jumia phone accessories from top brands like
+        Oraimo, Edifier, Samsung, Tecno, JBL, etc.
       </p>
       <p>
         We also have an amazing collection of top computing brands like Hp,
         Dell, Lenovo, Microsoft, Apple Macbook, and more! You can either buy
-        these computing devices as desktops or laptops depending on your choice.
-        Shop for computing accessories like printers, scanners, computer
-        components, and monitors to enhance your computer or laptop experience.
+        these computing devices as desktops or{" "}
+        <Link href="" className="underline font-semibold">
+          laptops
+        </Link>{" "}
+        depending on your choice. Shop for computing accessories like{" "}
+        <Link href="" className="underline font-semibold">
+          printers
+        </Link>
+        , scanners, computer components, and monitors to enhance your computer
+        or laptop experience.
       </p>
       <p>
         Our catalog of electronics offers you the highest quality of products
-        from trusted sellers and brands. You can get the latest smart
-        televisions and home audio devices for your living room. To spice things
-        up, you can get the PlayStation consoles or Xbox consoles especially if
-        you love gaming during your leisure time.
+        from trusted sellers and brands. You can get the latest smart{" "}
+        <Link href="" className="underline font-semibold">
+          televisions
+        </Link>{" "}
+        and home{" "}
+        <Link href="" className="underline font-semibold">
+          audio devices
+        </Link>{" "}
+        for your living room. To spice things up, you can get the{" "}
+        <Link href="" className="underline font-semibold">
+          PlayStation
+        </Link>{" "}
+        consoles or{" "}
+        <Link href="" className="underline font-semibold">
+          Xbox
+        </Link>{" "}
+        consoles especially if you love gaming during your leisure time.
       </p>
 
       <h2 className="font-semibold text-lg">
@@ -259,12 +293,26 @@ const ShoppingDestination = () => {
         Store
       </h2>
       <p>
-        There are limitless offers and deals on the Jumia Nigeria online store.
-        Enjoy up to 80% discount on selected items of the day during the flash
-        sales. We also have Half Price Deals where our shoppers buy selected
-        products like mobile phones, laptops, phone accessories, and clothing
-        pieces for half the original price. Get the best prices you can find on
-        the best deals page.
+        There are limitless offers and{" "}
+        <Link href="" className="underline font-semibold">
+          deals
+        </Link>{" "}
+        on the Jumia Nigeria online store. Enjoy up to 80% discount on selected
+        items of the day during the{" "}
+        <Link href="" className="underline font-semibold">
+          flash sales
+        </Link>
+        . We also have{" "}
+        <Link href="" className="underline font-semibold">
+          Half Price Deals
+        </Link>{" "}
+        where our shoppers buy selected products like mobile phones, laptops,
+        phone accessories, and clothing pieces for half the original price. Get
+        the best prices you can find on the{" "}
+        <Link href="" className="underline font-semibold">
+          best deals
+        </Link>{" "}
+        page.
       </p>
       <p>
         Enjoy other weekly offers that run all year long, which means that every
@@ -272,17 +320,26 @@ const ShoppingDestination = () => {
         discounts and deals that will help you save more and get the most
         affordable prices no one else can give you. To know more about these
         offers and deals, you can subscribe to our push notifications and
-        newsletters, read our blog, and follow us on all our social media
-        accounts.
+        newsletters, read our{" "}
+        <Link href="" className="underline font-semibold">
+          blog
+        </Link>
+        , and follow us on all our social media accounts.
       </p>
       <h2 className="font-semibold text-lg">
         Experience Fast Delivery and Online Shopping Convenience
       </h2>
       <p>
-        Get your cart delivered to you within 24 hrs when you buy items with the
-        Jumia Express tag, for selected products, you are also assured of free
-        delivery and have your products delivered to you at no extra cost! Also,
-        we have products that you can ship from abroad under the Jumia Global
+        Get your cart delivered to you within 24 hrs when you buy items with the{" "}
+        <Link href="" className="underline font-semibold">
+          Jumia Express
+        </Link>{" "}
+        tag, for selected products, you are also assured of free delivery and
+        have your products delivered to you at no extra cost! Also, we have
+        products that you can ship from abroad under the{" "}
+        <Link href="" className="underline font-semibold">
+          Jumia Global
+        </Link>{" "}
         catalog. This means that you can order various items from outside the
         country and get them delivered to your doorstep without hassles.
       </p>
@@ -303,9 +360,9 @@ const ShoppingDestination = () => {
       </h3>
       <p>
         Yes, you can return items you bought for free and get refunded as soon
-        as possible! We have a refund policy that grants you 7 days to return
-        ALL eligible items bought from the official store and other sellers on
-        Jumia Nigeria.
+        as possible! We have a refund policy that grants you 7 days to return{" "}
+        <span className="font-semibold">ALL</span> eligible items bought from
+        the official store and other sellers on Jumia Nigeria.
       </p>
       <h3 className="font-semibold text-sm">
         When is the Next Jumia Black Friday?
@@ -323,9 +380,9 @@ const ShoppingDestination = () => {
       </h3>
       <p>
         Customers in Lagos, Abuja, Ibadan,Warri,Benin, Abeokuta, Akure and
-        Portharcourt enjoy free delivery on items with the free delivery tag.
-        Free delivery does not apply to large items like refrigerators, huge
-        TVs, washing machines and others.
+        Portharcourt enjoy <span className="font-semibold">free delivery</span>{" "}
+        on items with the free delivery tag. Free delivery does not apply to
+        large items like refrigerators, huge TVs, washing machines and others.
       </p>
       <h3 className="font-semibold text-sm">
         What Is the Jumia Customer Care Line?
@@ -343,19 +400,26 @@ const ShoppingDestination = () => {
       </h3>
       <p>
         You can partner with Jumia as a seller on the website. All you need to
-        do is register and follow the necessary steps. With our large customer
-        base, we can help you reach a wider audience and sell many of your
-        products to our customers
+        do is{" "}
+        <Link href="" className="underline font-semibold">
+          register
+        </Link>{" "}
+        and follow the necessary steps. With our large customer base, we can
+        help you reach a wider audience and sell many of your products to our
+        customers
       </p>
       <h3 className="font-semibold text-sm">
         What Other Events are Celebrated in Jumia?
       </h3>
       <p>
-        Aside the Jumia Black Friday, the Jumia Tech Week is another big event
-        we celebrate in Jumia Nigeria. The goal of the Jumia Tech Week is to
-        help customers have access to the latest technology at the cheapest
-        prices they can get. It happens in the month of March of every year and
-        lasts at least two weeks.
+        Aside the Jumia Black Friday, the{" "}
+        <Link href="" className="underline font-semibold">
+          Jumia Tech Week
+        </Link>{" "}
+        is another big event we celebrate in Jumia Nigeria. The goal of the
+        Jumia Tech Week is to help customers have access to the latest
+        technology at the cheapest prices they can get. It happens in the month
+        of March of every year and lasts at least two weeks.
       </p>
       <p>
         Aside these major events, we also have Ramadan sales, Easter sales,
